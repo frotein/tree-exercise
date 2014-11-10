7bc67ae69317bd641d6443517a64571e298da13e
dean thurston <frotein@users.noreply.github.com>
2014-11-10 16:46:10 -0500

    Added class fot the tree
    Added a branch class that is the data structure of the tree, Has a value, a left and right node, and a link to its parent

--- tree.py
+++ tree.py
@@ -1,3 +1,18 @@
+#tree class, has a value, left and right nodes, and a link to its parent
+class branch:
+	def __init__(value, parent):
+		self.value = value
+		self.left = None
+		self.right = None
+		self.parent = parent
+
+	def addLeft(tree):
+		self.left = tree
+
+	def addRight(tree):
+		self.right = tree
+
+
 # input the number of levels
 levels = input("levels in tree: ")
 
