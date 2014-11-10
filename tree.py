d6520f2b1786ccd506a76bb622ea9e217ed93c25
dean thurston <frotein@users.noreply.github.com>
2014-11-10 17:56:38 -0500

    using lists
    instead of defining the tree as its own class, have each level of the tree represent by a list of numbers, 
    for example the top level is [1], the second level is [1 ,1] etc.

--- tree.py
+++ tree.py
@@ -1,3 +1,27 @@
+# takes in a list representation of a treeLevel and returns the list representation of the next level
+def computeNextLevel(tree):
+	lst = [] # the list being returned
+	if len(tree)  == 1 : # if this is the beginning, return the second level
+		lst.append(tree[0])
+		lst.append(tree[0])
+	else:
+		i = 0
+		while i < len(tree):
+
+			if i == 0: # the left edge is always 1
+				lst.append(1)
+				lst.append(tree[i] + tree[i + 1])
+			else:
+				if i == (len(tree) - 1): # the right edge is always 1
+					lst.append(tree[i - 1] + tree[i])
+					lst.append(1)
+				else: # computing middle is adding the values next to eachother
+					lst.append(tree[i - 1] + tree[i])
+					lst.append(tree[i] + tree[i + 1])
+
+			i = i + 1
+	return lst
+
 # input the number of levels
 levels = input("levels in tree: ")
 
@@ -5,7 +29,7 @@ levels = input("levels in tree: ")
 if not isinstance(levels , int) :
 	print "Error: input is not a positive Integer"
 else :
-	if levels < 0 :
+	if levels <= 0 :
 		print "Error: input is not a positive Integer"
 	
 
